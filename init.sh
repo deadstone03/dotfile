8b56236bce1686cb7795f3fa7f79df3160c6985b
Xing Dai <xingdai@xingdai-macbookpro2.roam.corp.google.com>
2017-03-01 20:51:07 -0800

    fix

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo apt-get -y install git
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln -s ~/.vimrc ~/dotfile/.vimrc
-ln -s ~/.tmux.conf ~/dotfile/.tmux.conf
+ln -s ~/dotfile/.vimrc ~/.vimrc
+ln -s ~/dotfile/.tmux.conf ~/.tmux.conf 
